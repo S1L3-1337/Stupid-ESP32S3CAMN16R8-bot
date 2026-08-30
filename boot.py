3c12dc4d64f0c3f4fe7a2d4dd3f910e1f1f284d6
S1L3NT1337 <s1l3ntd3v1337@gmail.com>
2026-08-31 00:52:59 +0330

    finally

--- boot.py
+++ boot.py
@@ -2,27 +2,25 @@ import ugit
 from machine import reset
 import random
 
-def run_ota():
-    try:
-        ugit.wificonnect()
-        dyn_branch = f"main?cb={random.randint(100_000, 999_999)}"
-        uflag = ugit.check_for_updates(isconnected=True, branch=dyn_branch)
-        if (uflag["new"] or uflag["changed"] or uflag["deleted"]):
-            print("[INFO] [OTA] New updates found. Downloading...")
-            ugit.backup()
-            ugit.pull_all(isconnected=True, branch=dyn_branch)
-            print("[INFO] [OTA] Update complete. Rebooting...")
-            reset()
-        else:
-            print("[INFO] [OTA] System is up to date.")
+try:
+    ugit.wificonnect()
+    dyn_branch = f"main?cb={random.randint(100_000, 999_999)}"
+    uflag = ugit.check_for_updates(isconnected=True, branch=dyn_branch)
+    if (uflag["new"] or uflag["changed"] or uflag["deleted"]):
+        print("[INFO] [OTA] New updates found. Downloading...")
+        ugit.backup()
+        ugit.pull_all(isconnected=True, branch=dyn_branch)
+        print("[INFO] [OTA] Update complete. Rebooting...")
+        reset()
+    else:
+        print("[INFO] [OTA] System is up to date.")
 
-    except Exception as e:
-        print(f"[ERROR] [OTA] Update operation failed: {e}")
-        print("[ERROR] [OTA] Restoring previous backup...")
-        try:
-            ugit.restore()
-            print("[INFO] [OTA] Backup restored successfully. Rebooting...")
-            reset()
-        except Exception as rollback_error:
-            print(f"[ERROR] [OTA] Rollback failed: {rollback_error}")
-run_ota()
+except Exception as e:
+    print(f"[ERROR] [OTA] Update operation failed: {e}")
+    print("[ERROR] [OTA] Restoring previous backup...")
+    try:
+        ugit.restore()
+        print("[INFO] [OTA] Backup restored successfully. Rebooting...")
+        reset()
+    except Exception as rollback_error:
+        print(f"[ERROR] [OTA] Rollback failed: {rollback_error}")
