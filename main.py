d404afe4429a05a7d75afde0c08a9739dcd5c8d8
S1L3NT1337 <s1l3ntd3v1337@gmail.com>
2026-08-30 22:59:12 +0330

    huuuuuuuuuuuuuuuh

--- main.py
+++ main.py
@@ -424,7 +424,7 @@ def handle_encoding(image_bytes): # --- BEGINNING OF AI-ASSISTED PART ---
 
 async def main():
     global latest_offset
-    print("[INFO] [MAIN] starting program...")
+    print("[INITIAL] [MAIN] starting program...")
     connect_wifi()
 
     print("[INFO] [MAIN] Bot is running. Listening for commands...")
