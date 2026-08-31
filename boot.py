40038453f7127f60f565b66cb41e69eb883cb1f2
S1L3NT1337 <s1l3ntd3v1337@gmail.com>
2026-08-31 03:41:27 +0330

    lululululululul

--- boot.py
+++ boot.py
@@ -1,7 +1,10 @@
 import ugit
 from machine import reset
+import gc
 
 try:
+    gc.collect()
+    gc.collect()
     ugit.wificonnect()
     uflag = ugit.check_for_updates(isconnected=True, branch="main")
     if (uflag["new"] or uflag["changed"] or uflag["deleted"]):
@@ -17,8 +20,10 @@ except Exception as e:
     print(f"[ERROR] [OTA] Update operation failed: {e}")
     print("[ERROR] [OTA] Restoring previous backup...")
     try:
-        ugit.restore()
-        print("[INFO] [OTA] Backup restored successfully. Rebooting...")
-        reset()
+        if ugit.restore():
+            print("[INFO] [OTA] Backup restored successfully. Rebooting...")
+            reset()
+        else:
+            raise Exception
     except Exception as rollback_error:
         print(f"[ERROR] [OTA] Rollback failed: {rollback_error}")
