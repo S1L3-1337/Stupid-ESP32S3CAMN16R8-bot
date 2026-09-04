501658b0690e6ef0464d0776c7c95a1b676c3c6a
S1L3NT1337 <s1l3ntd3v1337@gmail.com>
2026-09-04 19:39:18 +0330

    bababababaabababaababm

--- main.py
+++ main.py
@@ -454,8 +454,10 @@ def handle_encoding(image_bytes): # --- BEGINNING OF AI-ASSISTED PART ---
 async def main():
     print("[INITIAL] [WEBSOCKET] starting WEBSOCKET...")
     uasyncio.create_task(app.start_server(host='0.0.0.0', port=80))
+    print("[INFO] [MAIN] *--- LOADED HISTORY ---*")
     for log in rtc_json["boot_log"]:
         print(log)
+    print("[INFO] [MAIN] *--- LOADED HISTORY ---*")
 
     del logger.active_connections
 
